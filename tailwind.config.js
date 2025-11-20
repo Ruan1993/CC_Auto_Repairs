305ab8c4ae42f3605f76c5a07a96471db5a05077
Ruan <ruan.coetzee2@gmail.com>
2025-11-20 23:16:08 +0200

    Fixed Tailwind, changed design and button effects

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    './index.html',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#E0162B',
+        secondary: '#1A1A1A',
+        'dark-bg': '#121212',
+        'card-bg': '#1E1E1E',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
